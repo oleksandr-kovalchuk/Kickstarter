fa640d6a67aab1fc877c50973da4ebd8a2ac9426
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-03-13 17:56:34 +0200

    make build

---
+++ index.1a6d4b5c.js
@@ -0,0 +1,3 @@
+/* global alert */for(var e=document.querySelectorAll(".specs__link"),t=document.querySelector(".features__slider"),l=0;l<e.length;l++)e[l].addEventListener("click",function(){alert("The feature is still under development")});t.addEventListener("click",function(){alert("The feature is still under development")});//# sourceMappingURL=index.1a6d4b5c.js.map
+
+//# sourceMappingURL=index.1a6d4b5c.js.map
